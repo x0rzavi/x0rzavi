93408cc6cbfafc28fd1f024b146c868d6c34980d
Avishek Sen <x0rzavi@gmail.com>
2024-09-06 22:09:34 +0530

    Show gif from file

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def main():
 <picture>
     <source media="(prefers-color-scheme: dark)" srcset="output.gif">
     <source media="(prefers-color-scheme: light)" srcset="output.gif">
-    <img alt="GIFOS" src="{image.url}">
+    <img alt="GIFOS" src="output.gif">
 </picture>
 
 <sub><i>Generated automatically using [x0rzavi/github-readme-terminal](https://github.com/x0rzavi/github-readme-terminal) on {time_now}</i></sub>
