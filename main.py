a2cd660a61521ee8fe8129c55c32d88a780c598a
BlueKitsune狐 <saadshaan619@gmail.com>
2026-08-28 22:05:37 +0500

    Personalize GIF: Saad Yousuf, KITSUNE branding, vertical SAAD logo, Karachi timezone

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def main():
 
     t.gen_text("", 1, count=20)
     t.toggle_show_cursor(False)
-    year_now = datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%Y")
-    t.gen_text("GIF_OS Modular BIOS v1.0.11", 1)
-    t.gen_text(f"Copyright (C) {year_now}, \x1b[31mX0rzAvi Softwares Inc.\x1b[0m", 2)
+    year_now = datetime.now(ZoneInfo("Asia/Karachi")).strftime("%Y")
+    t.gen_text("KITSUNE Modular BIOS v1.0.11", 1)
+    t.gen_text(f"Copyright (C) {year_now}, \x1b[31mSad Softwares Inc.\x1b[0m", 2)
     t.gen_text("\x1b[94mGitHub Profile ReadMe Terminal, Rev 1011\x1b[0m", 4)
-    t.gen_text("Krypton(tm) GIFCPU - 250Hz", 6)
+    t.gen_text("Kitsune(tm) CPU - 250Hz", 6)
     t.gen_text(
         "Press \x1b[94mDEL\x1b[0m to enter SETUP, \x1b[94mESC\x1b[0m to cancel Memory Test",
         t.num_rows,
@@ -42,31 +42,33 @@ def main():
     t.gen_text("\x1b[96m", 1, count=0, contin=True)  # buffer to be removed
     t.set_font(FONT_FILE_LOGO, 66)
     # t.toggle_show_cursor(True)
-    os_logo_text = "GIF OS"
-    mid_row = (t.num_rows + 1) // 2
-    mid_col = (t.num_cols - len(os_logo_text) + 1) // 2
+    os_logo_text = "SAAD"
     effect_lines = gifos.effects.text_scramble_effect_lines(
         os_logo_text, 3, include_special=False
     )
-    for i in range(len(effect_lines)):
-        t.delete_row(mid_row + 1)
-        t.gen_text(effect_lines[i], mid_row + 1, mid_col + 1)
+    mid_row = (t.num_rows + 1) // 2
+    mid_col = t.num_cols // 2
+    start_row = mid_row - len(os_logo_text) // 2
+    for line in effect_lines:
+        for i, ch in enumerate(line):
+            t.delete_row(start_row + i + 1)
+            t.gen_text(ch, start_row + i + 1, mid_col + 1)
 
     t.set_font(FONT_FILE_BITMAP, 15)
     t.clear_frame()
     t.clone_frame(5)
     t.toggle_show_cursor(False)
-    t.gen_text("\x1b[93mGIF OS v1.0.11 (tty1)\x1b[0m", 1, count=5)
+    t.gen_text("\x1b[93mKITSUNE OS v1.0.11 (tty1)\x1b[0m", 1, count=5)
     t.gen_text("login: ", 3, count=5)
     t.toggle_show_cursor(True)
-    t.gen_typing_text("x0rzavi", 3, contin=True)
+    t.gen_typing_text("sad", 3, contin=True)
     t.gen_text("", 4, count=5)
     t.toggle_show_cursor(False)
     t.gen_text("password: ", 4, count=5)
     t.toggle_show_cursor(True)
     t.gen_typing_text("*********", 4, contin=True)
     t.toggle_show_cursor(False)
-    time_now = datetime.now(ZoneInfo("Asia/Kolkata")).strftime(
+    time_now = datetime.now(ZoneInfo("Asia/Karachi")).strftime(
         "%a %b %d %I:%M:%S %p %Z %Y"
     )
     t.gen_text(f"Last login: {time_now} on tty1", 6)
@@ -78,24 +80,24 @@ def main():
     t.delete_row(7, prompt_col)  # simulate syntax highlighting
     t.gen_text("\x1b[92mclear\x1b[0m", 7, count=3, contin=True)
 
-    ignore_repos = ["archiso-zfs", "archiso-zfs-archive"]
-    git_user_details = gifos.utils.fetch_github_stats("x0rzavi", ignore_repos)
-    user_age = gifos.utils.calc_age(26, 7, 2002)
+    ignore_repos = []
+    git_user_details = gifos.utils.fetch_github_stats("bluekitsune-sad", ignore_repos)
+    user_age = gifos.utils.calc_age(1, 1, 2004)
     t.clear_frame()
     top_languages = [lang[0] for lang in git_user_details.languages_sorted]
     user_details_lines = f"""
-    \x1b[30;101mx0rzavi@GitHub\x1b[0m
+    \x1b[30;101msad@GitHub\x1b[0m
     --------------
-    \x1b[96mOS:     \x1b[93mArch/Gentoo Linux, Windows 11, Android 14\x1b[0m
-    \x1b[96mHost:   \x1b[93mNetaji Subhash Engineering College \x1b[94m#NSEC\x1b[0m
-    \x1b[96mKernel: \x1b[93mComputer Science & Engineering \x1b[94m#CSE\x1b[0m
+    \x1b[96mOS:     \x1b[93mUbuntu, Kali, Arch, CachyOS\x1b[0m
+    \x1b[96mHost:   \x1b[93mIQRA University, Karachi \x1b[94m#IQRA\x1b[0m
+    \x1b[96mKernel: \x1b[93mComputer Science \x1b[94m#BSCS\x1b[0m
     \x1b[96mUptime: \x1b[93m{user_age.years} years, {user_age.months} months, {user_age.days} days\x1b[0m
-    \x1b[96mIDE:    \x1b[93mneovim, VSCode\x1b[0m
+    \x1b[96mIDE:    \x1b[93mneovim, VSCode, Android Studio\x1b[0m
     
     \x1b[30;101mContact:\x1b[0m
     --------------
-    \x1b[96mEmail:      \x1b[93mx0rzavi@gmail.com\x1b[0m
-    \x1b[96mLinkedIn:   \x1b[93msen-avishek\x1b[0m
+    \x1b[96mEmail:      \x1b[93msaadyousu64@gmail.com\x1b[0m
+    \x1b[96mLinkedIn:   \x1b[93msyed-saad-yousuf-raza\x1b[0m
     
     \x1b[30;101mGitHub Stats:\x1b[0m
     --------------
@@ -114,7 +116,7 @@ def main():
     t.gen_typing_text("\x1b[91mfetch.s", 1, contin=True)
     t.delete_row(1, prompt_col)
     t.gen_text("\x1b[92mfetch.sh\x1b[0m", 1, contin=True)
-    t.gen_typing_text(" -u x0rzavi", 1, contin=True)
+    t.gen_typing_text(" -u bluekitsune-sad", 1, contin=True)
 
     t.set_font(FONT_FILE_MONA, 16, 0)
     t.toggle_show_cursor(False)
@@ -163,10 +165,10 @@ def main():
 <picture>
     <source media="(prefers-color-scheme: dark)" srcset="./output.gif">
     <source media="(prefers-color-scheme: light)" srcset="./output.gif">
-    <img alt="GIFOS" src="output.gif">
+    <img alt="KITSUNE OS" src="output.gif">
 </picture>
 
-<sub><i>Generated automatically using [x0rzavi/github-readme-terminal](https://github.com/x0rzavi/github-readme-terminal) on {time_now}</i></sub>
+<sub><i>Generated automatically for <b>Saad Yousuf</b> ([bluekitsune-sad](https://github.com/bluekitsune-sad)) on {time_now}</i></sub>
 
 <!-- <details>
 <summary>More details</summary>
